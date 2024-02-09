42018953f1dbd55368fb13769c76c9d6dbffaaca
Jason Womack <jasonwomack@Jasons-MacBook-Pro.local>
2024-02-09 10:30:23 -0500

    dev 2nd

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 #[macro_use] extern crate rocket;
-
+use rocket::http::Status;
+use rocket::response::Redirect;
+use rocket::http::url::Orgin;
+use rocket::serde::json::(json, Value);
 #[get("/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+fn index() -> Redirect {
+    Redirect::to(uri!(call_api()))
 }
 
+//https://west-api.vapi.ai/assistant?limit=5
+#[get("https://west-api.vapi.ai/assistant")]
+fn call_api(_platfrom: &str){
+    Status::NoContent
+}
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![hello])
+    rocket::build()
+    .mount("/hello", routes![hello])
+    .mount("https://west-api.vapi.ai/assistant?limit=5", routes![call_api])
 }
